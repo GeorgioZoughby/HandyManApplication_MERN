bbb3b15e25b2a0396a712dba7c3136ca00ea81a2
GeorgioZoughby <georgio.zoghby@outlook.com>
2024-11-12 21:38:19 +0200

    Home page sample

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,9 +1,13 @@
-function Message() {
-    return (
-        <div>
-        <h1>Welcome to the Home</h1>
-        </div>
-    );
-}
+import React from 'react';
+
+const Message = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the Home page of your application.</p>
+    </div>
+  );
+};
 
 export default Message;
+
